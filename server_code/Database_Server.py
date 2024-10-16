e0375bfcbf2dccc2d3cac604198f862f4570c1c8
merlin.tschann@student.htl-rankweil.at <merlin.tschann@student.htl-rankweil.at>
2024-10-16 09:45:01 +0000

    Edited Database_Server

--- server_code/Database_Server.py
+++ server_code/Database_Server.py
@@ -4,7 +4,7 @@ from anvil.tables import app_tables
 import anvil.files
 from anvil.files import data_files
 import anvil.server
-import sqlite3
+import sqlite3 
 
 # This is a server module. It runs on the Anvil server,
 # rather than in the user's browser.
@@ -15,13 +15,37 @@ import sqlite3
 #
 @anvil.server.callable
 def say_hello(name):
-   print("Hello, " + name + "!")
-   return 42
-  
+  print("Hello, " + name + "!")
+  return 42
+
 @anvil.server.callable
-def get_jugendherbergen(rows="*"):
-  conn = sqlite3.connect(data_files['jugendherbergen_verwaltung.db'])
-  cursor = conn.cursor()
-  res = list(cursor.execute(f"SELECT {rows} FROM jugendherbergen"))
-  print(res)
-  return res
+def get_jugendherberge():
+    # Read the contents of a file
+    
+    conn = sqlite3.connect(data_files["jugendherbergen_verwaltung.db"])
+    cuursor = conn.cursor()
+    res = list(cuursor.execute("SELECT name, JID FROM jugendherbergen"))
+    print(res)
+    return res
+
+@anvil.server.callable
+def get_zimmer():
+    conn = sqlite3.connect(data_files["jugendherbergen_verwaltung.db"])
+    cuursor = conn.cursor()
+    res = list(cuursor.execute("SELECT CAST(bettenanZahl AS TEXT), ZID FROM zimmer"))
+    
+    print(res)
+    
+    return res
+
+
+@anvil.server.callable
+def get_zimmer_jugendherberge():
+    conn = sqlite3.connect(data_files["jugendherbergen_verwaltung.db"])
+    cuursor = conn.cursor()
+    res = list(cuursor.execute('''
+    SELECT z.ZID, z.bettenanZahl, j.name
+    FROM zimmer z
+    JOIN jugendherbergen j ON z.herbergeFK = j.JID'''))
+
+    print(res)
