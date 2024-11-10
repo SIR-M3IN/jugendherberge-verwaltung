420debc3b472e466b1e5b254f13614d7708c025a
merlin.tschann@student.htl-rankweil.at <merlin.tschann@student.htl-rankweil.at>
2024-11-10 11:29:28 +0000

    Created Startseite.RowTemplate5, Startseite.RowTemplate4 and Startseite.RowTemplate6; Edited Startseite

--- client_code/Startseite/__init__.py
+++ client_code/Startseite/__init__.py
@@ -16,3 +16,15 @@ class Startseite(StartseiteTemplate):
   def Benutzer_DropDown_change(self, **event_args):
     selected_user = self.Benutzer_DropDown.selected_value
     self.room_drop_down.items = anvil.server.call('get_zimmer_with_preisklasse', selected_user)
+
+  def start_date_picker_change(self, **event_args):
+    """This method is called when the selected date changes"""
+    pass
+
+  def end_date_picker_change(self, **event_args):
+    """This method is called when the selected date changes"""
+    pass
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
